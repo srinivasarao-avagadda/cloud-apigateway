fc512248b11158ccc8a2f9d1b650891b7ba15438
srinivasarao.avagadda <srinivasacse@gmail.com>
2021-09-01 01:09:10 +0530

    Initial Commit

---
+++ src/main/java/com/mycompany/CloudApigatewayApplication.java
@@ -0,0 +1,15 @@
+package com.mycompany;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class CloudApigatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudApigatewayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mycompany/CloudApigatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mycompany;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudApigatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
